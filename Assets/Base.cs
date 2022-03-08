a72952f4ec2a8cb0b78bb1f609b023ffd4ae28f6
Daniel Di Nardi <c18487682@mytudublin.ie>
2022-03-08 09:57:21 +0000

    Fighter spawning & base accumulating tiberium

--- Assets/Base.cs
+++ Assets/Base.cs
@@ -11,6 +11,15 @@ public class Base : MonoBehaviour
 
     public GameObject fighterPrefab;
 
+    IEnumerator MineTiberium()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+
+            tiberium = tiberium + 1;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +28,25 @@ public class Base : MonoBehaviour
         {
             r.material.color = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
         }
+
+        StartCoroutine(MineTiberium());
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(tiberium >= 10)
+        {
+            tiberium = tiberium - 10;
+
+            foreach (Renderer r in fighterPrefab.GetComponentsInChildren<Renderer>())
+            {
+                r.material = GetComponent<Renderer>().material;
+            }
+
+            Instantiate(fighterPrefab, transform.position, transform.rotation);
+        }
+
         text.text = "" + tiberium;
     }
 }
---
+++ Assets/Fighter.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighter : MonoBehaviour
+{
+    public float tiberium = 0;
+
+    public GameObject bulletPrefab;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
